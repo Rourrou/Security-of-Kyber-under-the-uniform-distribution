cd9043e9f82d92424f6948087554208b6114fd28
Your Name <you@example.com>
2025-01-21 14:50:07 +0800

    Section 4

--- README.md
+++ README.md
@@ -2,20 +2,12 @@
 https://eprint.iacr.org/2024/1979
 
 Section 3. Kyber hardness
-1. compute the hardness of Kyber and its variant under the uniform distribution.
-
-    set the distr parameter in MLWE_security with "binomial" or "uniform".
-
-    run Kyber_hardness.py.
+1. Compute the hardness of Kyber and its variant under the uniform distribution: set the distr parameter in MLWE_security with "binomial" or "uniform" and run Kyber_hardness.py
 
 Section 4. Decryption failure attack
-1. compute the decryption failure probability of Kyber and its variant under the uniform distribution.
-
-    run Kyber_failure.py
-2. collecting the failures
-   
-3. Identify the sign and position of failure
-
-    run
+1. Compute the decryption failure probability of Kyber and its variant under the uniform distribution: run Kyber_failure.py
+2. Collect the failures: the decryption failures are collected as Kyber512_uniform/decryption_failures_33322_all.txt
+3. Identify the sign and position of failure: run df_position.py
+4. Recovering the secret key using decryption failures: run key_recovery.py 
     
 
--- Section 4. Decryption failure attack/df_position.py
+++ Section 4. Decryption failure attack/df_position.py
@@ -86,6 +86,7 @@ def epp_dis():
     plt.grid(axis='y', alpha=0.75)
     plt.show()
 
+
 # describe the correlation of ff & fs
 def cor_df():
     A = np.loadtxt("Kyber512_uniform/failures/A.txt", dtype=np.int16)
@@ -99,7 +100,7 @@ def cor_df():
         if sign[i] == 0:
             A_pos[i] = -A_pos[i]
 
-    test_num = 10000
+    test_num = 1000000
     # test_num = 100
     ff = []
     fs = []
@@ -148,13 +149,14 @@ def cor_df():
     ax.set_ylabel('probability', fontdict={'family': 'Times New Roman', 'size': 14})
     plt.show()
 
-# recover the relative position of dfi with df0
-def pos_df2():
+
+# Identifying the position of decryption failure using method in D'Anvers et al
+def pos_df0():
     A = np.loadtxt("Kyber512_uniform/failures/A.txt", dtype=np.int16)
     m, n = A.shape
     print("m=", m, "n=", n)
 
-    test_num = 100000
+    test_num = 10000
     suc_num = 0
     for i in range(test_num):
         print(f"\rProgress: {i+1}/{test_num}", end='')
@@ -176,99 +178,24 @@ def pos_df2():
         if max_pos_r == 0:
             suc_num += 1
         # print(max_inn, i, max_pos_r)
-    print("\nexperimental probability of combining 2 ciphertext, successpro %f" %(float(suc_num / test_num)))
-
-
-# recover the relative position of dfi with dfs
-def pos_dfs():
-    A = np.loadtxt("Kyber512_uniform\A.txt", dtype=np.int16)
-    A_pos = A.copy()
-    sign = np.loadtxt("Kyber512_uniform\sign.txt", dtype=np.int16)
-    m, n = A.shape
-    print("m=", m, "n=", n)
-    for i in range(m):
-        if sign[i] == 0:
-            A_pos[i] = -A_pos[i]
-    print(A_pos)
-
-    sum_num = 2
-    # calculate the success probability
-    suc_pro = 0
-    for test in range(100):
-        # 从0到cip_num之间随机选择sum_num个不同的数
-        rn = random.sample(range(m), sum_num)
-        # 将这sum_num个不同的向量加起来，当作密钥估计的初始值
-        b_1 = A_pos[rn[0]].copy()
-        b_1 = b_1 / np.linalg.norm(b_1)
-        for i in range(1, sum_num):
-            # b_1 += A_pos[rn[i]]
-            b_1 += A_pos[rn[i]] / np.linalg.norm(A_pos[rn[i]])
-        print(b_1)
-
-        del_pos = []
-        del_rot = []
-        for i in range(sum_num):
-            del_pos.append(rn[i])
-            del_rot.append(0)
-
-        suc_num1 = 0
-        for ite in range(100):
-            # print("del_pos = ", del_pos)
-            # print("del_rot = ", del_rot)
-            max_inn = 0
-            max_pos_i = 0
-            max_pos_r = 0
-            for i in range(m):  # For each rotation of vec
-                if i not in del_pos:
-                    for r in range(256):
-                        inn = abs(np.dot(b_1, mod_rotation(A_pos[i], r, 256)))
-                        if inn > max_inn:
-                            max_inn = inn
-                            max_pos_i = i
-                            max_pos_r = r
-            # b_1 += mod_rotation(A_pos[max_pos_i], max_pos_r, 256)
-            b_1 += mod_rotation(A_pos[max_pos_i], max_pos_r, 256) / np.linalg.norm(A_pos[max_pos_i])
-            del_pos.append(max_pos_i)
-            del_rot.append(max_pos_r)
-            if max_pos_r == 0:
-                suc_num1 += 1
-            print(max_inn, max_pos_i, max_pos_r)
-        print("the", test + 1, "test's success number1 is", suc_num1)
-
-        suc_num2 = 0
-        for ite in range(100):
-            max_inn = 0
-            max_pos_r = 0
-            for r in range(256):
-                inn = abs(np.dot(b_1, mod_rotation(A_pos[del_pos[ite]], r, 256)))
-                if inn > max_inn:
-                    max_inn = inn
-                    max_pos_r = r
-            if max_pos_r == 0:
-                suc_num2 += 1
-            print(max_inn, max_pos_r)
-
-        print("the", test + 1, "test's success number2 is", suc_num2)
-        if suc_num2 >= 90:
-            suc_pro += 1
-    print("the success probability with", sum_num, "number df is", suc_pro / 100)
+    print("\nexperimental probability of identifying the position of decryption failure using method in D'Anvers et al, "
+          "successpro %f" %(float(suc_num / test_num)))
 
 
 # recover the relative position of dfi with dfs by iterative
-def pos_dfs_ite():
-    # A = np.loadtxt("Kyber512_uniform/33322/2000/A.txt", dtype=np.int16)
-    A = np.loadtxt("Kyber512_Gaussian/A.txt", dtype=np.int16)
+def pos_dfs_ite(known_num):
+    A = np.loadtxt("Kyber512_uniform/3000/A.txt", dtype=np.int16)
     A_pos = A.copy()
-    sign = np.loadtxt("Kyber512_Gaussian/b_1.txt", dtype=np.int16)
+    sign = np.loadtxt("Kyber512_uniform/3000/b.txt", dtype=np.int16)
     m, n = A.shape
-    m = 10
+    #m = 10
     print("m=", m, "n=", n)
     for i in range(m):
         if sign[i] == 0:
             A_pos[i] = -A_pos[i]
     print(A_pos)
 
-    sum_num = 1
+    sum_num = known_num
     ite_num = 3
     # calculate the success probability
     suc_pro = 0
@@ -348,19 +275,17 @@ if __name__ == '__main__':
     sign_match()
 
     # describe the distribution of |G[i]| of df
-    # epp_dis()
+    epp_dis()
 
     # describe the correlation of ff & fs
-    # cor_df()
+    cor_df()
 
     # recover the relative position of dfi with df0
-    # pos_df2()
-
-    # recover the relative position of dfi with dfs
-    # pos_dfs()
+    pos_df0()
 
     # recover the relative position of dfi with dfs by iterative
-    # pos_dfs_ite()
+    for i in range(1, 5, 1):
+        pos_dfs_ite(i)
 
 
 
